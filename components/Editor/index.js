ca8c059abddb39bec167a78174e8e4aec1cfab3a
Veliko <Kardziev>
2023-06-29 10:11:08 +0200

    handle Change in Textarea

--- components/Editor/index.js
+++ components/Editor/index.js
@@ -1,11 +1,12 @@
-export default function Editor({ defaultText }) {
+export default function Editor({ defaultText, handleChange }) {
 	return (
 		<section>
 			<textarea
 				type="text"
 				id="editor"
 				rows="14"
-				defaultValue={defaultText}></textarea>
+				defaultValue={defaultText}
+				onChange={handleChange}></textarea>
 		</section>
 	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import Editor from "../../components/Editor";
 import Preview from "../../components/Preview";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({ defaultText }) {
+export default function Home({ defaultText, handleChange }) {
 	return (
 		<>
 			<Head>
@@ -16,7 +14,7 @@ export default function Home({ defaultText }) {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Editor defaultText={defaultText}></Editor>
+			<Editor defaultText={defaultText} handleChange={handleChange}></Editor>
 			<Preview defaultText={defaultText}></Preview>
 		</>
 	);
